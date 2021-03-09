e0483e06212d6b88e96c8e43039ded3805728113
Владимир Горин <vgor555@gmail.com>
2021-03-09 23:02:03 +0300

    добавлены компоненты App, Converter

--- src/app.vue
+++ src/app.vue
@@ -1,14 +1,51 @@
 <template>
-  <div></div>
+  <div class="container">
+    <header>Exchange Rates Widget</header>
+    <main>
+      <converter :title="title" />
+    </main>
+    <footer>&copy; Copyright {{ new Date().getFullYear() }}</footer>
+  </div>
 </template>
 
 <script>
+import Converter from "./components/Converter.vue";
 export default {
+  name: "App",
+  components: {
+    Converter,
+  },
   data() {
-    return {};
+    return {
+      title: "Exchange Rates",
+    };
   },
 };
 </script>
 
 <style lang="scss">
+$primary: #6893d3;
+
+$main-font: Arial, "Helvetica Neue", Helvetica, sans-serif;
+
+html,
+body {
+  margin: 0;
+  padding: 0;
+  font-family: $main-font;
+  font-size: 14px;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+header,
+footer {
+  padding: 15px 30px;
+  background: $primary;
+}
+main {
+  flex: 1;
+}
 </style>
---
+++ src/components/Converter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="converter">
+    <h3>{{ title }}</h3>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Converter",
+  props: {
+    title: { type: String, default: "" },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.converter {
+  width: 300px;
+  max-width: 100%;
+  padding: 30px;
+}
+</style>
