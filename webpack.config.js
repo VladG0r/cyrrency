6b0a9c4ac5200f67ad922cf3e193e2afa123cdb6
Владимир Горин <vgor555@gmail.com>
2021-03-09 22:09:25 +0300

    добавляем scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,23 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
 module.exports = {
-  entry: './src/main.js',
+  entry: "./src/main.js",
   module: {
     rules: [
-      { test: /\.js$/, use: 'babel-loader' },
-      { test: /\.vue$/, use: 'vue-loader' },
-      { test: /\.css$/, use: ['vue-style-loader', 'css-loader'] },
-    ]
+      { test: /\.js$/, use: "babel-loader" },
+      { test: /\.vue$/, use: "vue-loader" },
+      { test: /\.css$/, use: ["vue-style-loader", "css-loader"] },
+      {
+        test: /\.scss$/,
+        use: ["vue-style-loader", "css-loader", "sass-loader"],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: "./src/index.html",
     }),
     new VueLoaderPlugin(),
-  ]
+  ],
 };
